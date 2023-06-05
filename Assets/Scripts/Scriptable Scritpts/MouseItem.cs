b4a86b100bdb8005578973bcbb956d71347c79fc
kleberkgns <kleber.silva.kgns@gmail.com>
2023-06-05 14:56:00 -0300

    Gestão dos Itens: Lesson 02 | Mostrando o Ícone no Ponteiro

---
+++ Assets/Scripts/Scriptable Scritpts/MouseItem.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseItem : MonoBehaviour
+{
+    public GameObject itemIcon;
+    public ItemSlot slotClicked;
+    public ItemSlot slotHovered;
+}
